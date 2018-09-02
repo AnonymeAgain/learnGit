8b29d8ffa46d92c25ab14514e0054f78b976ec3f
vbird <youaremycupoftea@sina.cn>
2018-09-02 15:46:43 +0800

    jj

---
+++ helloword.java
@@ -0,0 +1,13 @@
+
+public class helloword {
+
+	public helloword(){}	
+	
+	public static void main(String[] arg){
+		System.out.println("add one row ");
+	
+		System.out.println("helloword");
+	}
+	
+	
+}
